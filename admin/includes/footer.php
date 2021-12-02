8c4fa7db89f7b78a29bd594ba0d72ed5d500046b
Vamford Raymundo <rvamford@gmail.com>
2021-12-02 14:49:56 +0800

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div style="text-align: center" class="">
+      <b>All rights reserved</b>
+    </div>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="container">
+      <div style="text-align: center" class="">
+        <b>All rights reserved</b>
+      </div>
+    </div>
+</footer>
